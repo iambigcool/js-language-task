aa212f9ccb8e65fb06546a63ae66a758f262dfed
IamRicky <lmx244620@gmail.com>
2023-01-25 00:08:49 +1300

    Add part2 page151 task

---
+++ part2/page151-why-return-false-doesn't-work/page151-why-return-false-doesn't-work.js
@@ -0,0 +1,20 @@
+// Why in the code below doesn’t work at all?
+// function handler() {
+//   alert( "..." );
+//   return false;
+// }
+// <a href="https://w3.org" onclick="handler()">the browser will go to w3.org</a>
+// The browser follows the URL on click, but we don’t want it. How to fix?
+// answer:
+// onclick="return handler()"
+
+
+
+// Make all links inside the element with id="contents" ask the user if they really want to leave. And if they don’t then don’t follow.
+// https://plnkr.co/edit/2q3DhC51iVEYU3Ht?p=preview&preview
+// see catch-link.html
+
+
+// Create an image gallery where the main image changes by the click on a thumbnail.
+// https://plnkr.co/edit/b8rcZiN2kqsbPyT0?p=preview&preview
+// see gallery.html
