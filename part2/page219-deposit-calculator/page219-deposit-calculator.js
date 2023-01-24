74e2069609a0c5bdfc78757211483abb438d8241
IamRicky <lmx244620@gmail.com>
2023-01-25 08:24:57 +1300

    Add part2 page 219 task

---
+++ part2/page219-deposit-calculator/page219-deposit-calculator.js
@@ -0,0 +1,3 @@
+// Create an interface that allows to enter a sum of bank deposit and percentage, then calculates how much it will be after given periods of time.
+// https://plnkr.co/edit/m91aRhjRUmEfzctR?p=preview&preview
+// see calc.html
