b38dba1e1e98f1b97522a1390bdea8e6e9bc8d3c
IamRicky <lmx244620@gmail.com>
2023-01-25 08:37:17 +1300

    Add part2 page275 task

---
+++ part2/page275-what-will-be-the-output-of-this-code/page275-what-will-be-the-output-of-this-code.js
@@ -0,0 +1,7 @@
+// What will be the output of this code?
+// console.log(1);
+// setTimeout(() => console.log(2));
+// Promise.resolve().then(() => console.log(3)); Promise.resolve().then(() => setTimeout(() => console.log(4))); Promise.resolve().then(() => console.log(5));
+// setTimeout(() => console.log(6));
+// console.log(7);
+1, 7, 3, 5, 4, 2, 6 // wrong correct is 1, 7, 3, 5, 2, 6, 4
