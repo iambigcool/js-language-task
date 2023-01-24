ee208f563f2c3aed05c2be52886c3d171c054043
IamRicky <lmx244620@gmail.com>
2023-01-25 07:47:05 +1300

    Add part2 page182 task

---
+++ part2/page182-drag-superheroes-around-the-field/page182-drag-superheroes-around-the-field.js
@@ -0,0 +1,9 @@
+// Create a slider:
+// https://plnkr.co/edit/p7Ag21yFCKk3iLFe?p=preview&preview
+// see slider.html
+
+
+
+// Make all elements with class draggable – draggable. Like a ball in the chapter.
+// https://plnkr.co/edit/lAng5DJhG41lE78n?p=preview&preview
+// Todo
