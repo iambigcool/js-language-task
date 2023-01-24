ea4b9a1e2c68bd8d5b09e1d670a2da1364f798d1
IamRicky <lmx244620@gmail.com>
2023-01-25 07:29:30 +1300

    Add Part2 page173 task

---
+++ part2/page173-improved-tooltip-behavior/page173-improved-tooltip-behavior.js
@@ -0,0 +1,11 @@
+// Write JavaScript that shows a tooltip over an element with the attribute data-tooltip. The value of this attribute should become the tooltip text.
+// https://plnkr.co/edit/N8Y84iRZJK3JdVjW?p=preview&preview
+// see improved-tooltip.html
+
+
+
+
+// Write a function that shows a tooltip over an element only if the visitor moves the mouse to it, but not through it.
+// https://plnkr.co/edit/vaC7qQoEslRkWWZ7?p=preview&preview
+// correct answer is:
+// https://plnkr.co/edit/n8OtYNISvRNcwCcm?p=preview&preview
